5314c5af08a078179d6ad30ae05c5e791a6702fd
Asankilp <60691961+Asankilp@users.noreply.github.com>
2021-04-11 12:58:22 +0800

    Create test.py

---
+++ test.py
@@ -0,0 +1,11 @@
+print "Content-type:text/html"
+print 
+print '<html>'
+print '<head>'
+print '<meta charset="utf-8">'
+print '<title>1</title>'
+print '</head>'
+print '<body>'
+print '<h2>test</h2>'
+print '</body>'
+print '</html>'
